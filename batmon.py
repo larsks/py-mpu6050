b0c3a423972d81e7c3d575cfe281a1d820745396
Lars Kellogg-Stedman <lars@redhat.com>
2017-02-10 23:30:25 -0500

    added code to activate esp battery monitoring

---
+++ batmon.py
@@ -0,0 +1,28 @@
+# from https://github.com/micropython/micropython/issues/2352
+
+import esp
+from flashbdev import bdev
+import machine
+
+ADC_MODE_VCC = 255
+ADC_MODE_ADC = 0
+
+def set_adc_mode(mode):
+    sector_size = bdev.SEC_SIZE
+    flash_size = esp.flash_size() # device dependent
+    init_sector = int(flash_size / sector_size - 4)
+    data = bytearray(esp.flash_read(init_sector * sector_size, sector_size))
+    if data[107] == mode:
+        return  # flash is already correct; nothing to do
+    else:
+        data[107] = mode  # re-write flash
+        esp.flash_erase(init_sector)
+        esp.flash_write(init_sector * sector_size, data)
+        print("ADC mode changed in flash; restart to use it!")
+        return
+
+def adc_reads_vcc():
+    set_adc_mode(ADC_MODE_VCC)
+
+def adc_reads_analog():
+    set_adc_mode(ADC_MODE_ADC)
